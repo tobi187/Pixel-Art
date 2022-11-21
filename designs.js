5229c66a32eb2f581436e2681d8e5cf8bdf74335
tobi <fischerstobi1998@gmail.com>
2022-11-21 20:42:58 +0100

    made it working

--- designs.js
+++ designs.js
@@ -1,128 +1,113 @@
-const sizePicker = document.querySelector('.size-picker');
-const pixelCanvas = document.querySelector('.pixel-canvas');
+const sizePicker = document.querySelector('#size-picker');
+const pixelCanvas = document.querySelector('#pixel-canvas');
 const quickFill = document.querySelector('.quick-fill');
-const eraseMode = document.querySelector('.erase-mode');
-const drawMode = document.querySelector('.draw-mode');
 
 function makeGrid() {
-  let gridHeight = document.querySelector('.input-height').value;
-  let gridWidth = document.querySelector('.input-width').value;
-  while (pixelCanvas.firstChild) {
-    pixelCanvas.removeChild(pixelCanvas.firstChild);
-    }
-  for (let i = 1;) i <= gridHeight; i++) {
+  let gridHeight = document.querySelector('#input-height').value;
+  let gridWidth = document.querySelector('#input-width').value;
+  pixelCanvas.innerHTML = "";
+  
+  for (let i = 0; i < gridHeight; i++) {
     let gridRow = document.createElement('tr');
     pixelCanvas.appendChild(gridRow);
-    for (let j = 1; j <= gridWidth; j++) {
+    
+    for (let j = 0; j < gridWidth; j++) {
       let gridCell = document.createElement('td');
-      gridRow.appendChild(gridCell);
-      gridCell.addEventListener('mousedown', function() {
-        const color = document.querySelector('.color-picker').value;
-        this.style.backgroundColor = color;
+      gridCell.addEventListener('click', function(e) {
+        e.target.style.backgroundColor = document.querySelector('#color-picker').value;
       })
+      gridRow.appendChild(gridCell);
     }
   }
 }
 
-makeGrid(25, 25);
-sizePicker.addEventListener('sumbit', function(e) {
+sizePicker.addEventListener('submit', function(e) {
   e.preventDefault();
   makeGrid();
 });
 
-let down = false;
+// let down = false;
 
-pixelCanvas.addEventListener('mousedown', function(e) {
-  down = true;
-  pixelCanvas.addEventListener('mouseup', function() {
-    down = false;
-  });
+// pixelCanvas.addEventListener('mousedown', function(e) {
+//   down = true;
+//   pixelCanvas.addEventListener('mouseup', function() {
+//     down = false;
+//   });
 
-  pixelCanvas.addEventListener('mouseleave', function() {
-    down =false;
-  });
+//   pixelCanvas.addEventListener('mouseleave', function() {
+//     down = false;
+//   });
 
-  pixelCanvas.addEventListener('mouseover', function(e) {
+//   pixelCanvas.addEventListener('mouseover', function(e) {
 
-    const color = document.querySelector('color-picker').value;
-    if (down) {
-      if (e.target,tagName === 'TD') {
-        e.target.style.backgroundColor = color;
-      }
-    }
-  });
-});
-quickFill.addEventListener('click', function(e) {
-  e.preventDefault();
-  const color = document.querySelector('.color-picker').value;
-  pixelCanvas.querySelectorAll('td').forEach(td => td.style.backgroundColor = color);
-});
+//     const color = document.querySelector('#color-picker').value;
+//     if (down) {
+//       if (e.target,tagName === 'TD') {
+//         e.target.style.backgroundColor = color;
+//       }
+//     }
+//   });
+// });
 
-pixelCanvas.addEventListener('dbclick', e => {
-  e.target.style.backgroundColor = null;
-});
+// quickFill.addEventListener('click', function(e) {
+//   e.preventDefault();
+//   const color = document.querySelector('#color-picker').value;
+//   pixelCanvas.querySelectorAll('td').forEach(td => td.style.backgroundColor = color);
+// });
 
-eraseMode.addEventListener('click', function() {
-  pixelCanvas.addEventListener('mousedown', function(e) {
-  	down = true;
-  	pixelCanvas.addEventListener('mouseup', function() {
-  		down = false;
-  	});
+// pixelCanvas.addEventListener('dblclick', e => {
+//   e.target.style.backgroundColor = null;
+// });
 
-    pixelCanvas.addEventListener('mouseleave', function() {
-      down = false;
-    });
-    pixelCanvas.addEventListener('mouseover', function(e) {
+// eraseMode.addEventListener('click', function() {
+//   pixelCanvas.addEventListener('mousedown', function(e) {
+//   	down = true;
+//   	pixelCanvas.addEventListener('mouseup', function() {
+//   		down = false;
+//   	});
 
-      if (down) {
-        if (e.target.tagName === 'TD') {
-        	e.target.style.backgroundColor = null;
-        }
-      }
-    });
-  });
+//     pixelCanvas.addEventListener('mouseleave', function() {
+//       down = false;
+//     });
+//     pixelCanvas.addEventListener('mouseover', function(e) {
 
-  pixelCanvas.addEventListener('mousedown', function(e) {
-    e.target.style.backgroundColor = null;
-  });
-});
-drawMode.addEventListener('click', function() {
-  pixelCanvas.addEventListener('mousedown', function(e) {
-  	down = true;
-  	pixelCanvas.addEventListener('mouseup', function() {
-  		down = false;
-  	});
+//       if (down) {
+//         if (e.target.tagName === 'TD') {
+//         	e.target.style.backgroundColor = null;
+//         }
+//       }
+//     });
+//   });
 
-    pixelCanvas.addEventListener('mouseleave', function() {
-      down = false;
-    });
-    pixelCanvas.addEventListener('mouseover', function(e) {
-      const color = document.querySelector('.color-picker').value;
+//   pixelCanvas.addEventListener('mousedown', function(e) {
+//     e.target.style.backgroundColor = null;
+//   });
+// });
 
-    	if (down) {
-        if (e.target.tagName === 'TD') {
-        	e.target.style.backgroundColor = color;
-        }
-      }
-    });
-  });
+// drawMode.addEventListener('click', function() {
+//   pixelCanvas.addEventListener('mousedown', function(e) {
+//   	down = true;
+//   	pixelCanvas.addEventListener('mouseup', function() {
+//   		down = false;
+//   	});
 
-  pixelCanvas.addEventListener('mousedown', function(e) {
-    if (e.target.tagName !== 'TD') return;
-    const color = document.querySelector('.color-picker').value;
-    e.target.style.backgroundColor = color;
-  });
-});
+//     pixelCanvas.addEventListener('mouseleave', function() {
+//       down = false;
+//     });
+//     pixelCanvas.addEventListener('mouseover', function(e) {
+//       const color = document.querySelector('#color-picker').value;
 
+//     	if (down) {
+//         if (e.target.tagName === 'TD') {
+//         	e.target.style.backgroundColor = color;
+//         }
+//       }
+//     });
+//   });
 
-
-
-
-
-
-
-
-
-
-
-}
+//   pixelCanvas.addEventListener('mousedown', function(e) {
+//     if (e.target.tagName !== 'TD') return;
+//     const color = document.querySelector('#color-picker').value;
+//     e.target.style.backgroundColor = color;
+//   });
+// });
